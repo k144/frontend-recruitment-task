1eff72664045b5b26a1c2104619819b3b135341c
Jakub Nowak <jakubnowak712@gmail.com>
2022-06-07 21:43:11 +0200

    add basic click counting

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,19 @@
 let popupEl = document.getElementById("popup");
 let clickCountEl = document.getElementById("click-count");
 
+let clickCount = 0;
+
 function openPopup() {
+    clickCount++;
+    updatePopupText();
     popupEl.classList.remove("hidden");
 }
 
 function closePopup() {
     popupEl.classList.add("hidden");
+}
+
+function updatePopupText() {
+    clickCountEl.innerText = clickCount;
+    console.log("tst")
 }
